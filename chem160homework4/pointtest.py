53b5fbbecb2aa7c31d386056272046a41c4be9db
nhukim35 <54655601+nhukim35@users.noreply.github.com>
2019-09-29 23:39:45 -0700

    Add files via upload

---
+++ chem160homework4/pointtest.py
@@ -0,0 +1,10 @@
+from point import *
+p1=point(3,[1., 2., 3.])
+p1
+p1.scale(3.3)
+print(p1)
+p2=point(3,[6.,7.,8.])
+print("p1 add p2=", p1.add(p2))
+print("p1 sqmag p2=", p1.sqmag(p2))
+      
+      
